84efa48e6215c638b759e3fd82f1f77b2a2c1e22
aikansh <aikanshgarg0303@gmail.com>
2023-12-05 03:05:55 +0530

    added preloader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import { useState } from "react";
 import "./styles/App.css";
 import ProductList from "./components/ProductList.js";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="info-text">
-        Assignment submission of{" "}
-        <a
-          href="https://docs.google.com/document/d/19AUvxUtQ2mPuqFrM7GP9VeivFyobqzYDUOSeFFAWokI/edit"
-          target="_blank"
-        >
-          Andisor's frontend task
-        </a>{" "}
-        by{" "}
-        <a href="https://www.linkedin.com/in/aikansh-garg/" target="_blank">
-          Aikansh Garg.
-        </a>
-      </div>
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById("spinner");
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = "none";
+      setLoading(false);
+    }, 2000);
+  }
 
-      <ProductList />
-    </div>
+  return (
+    !loading && (
+      <div className="App">
+        <div className="info-text">
+          Assignment submission of{" "}
+          <a
+            href="https://docs.google.com/document/d/19AUvxUtQ2mPuqFrM7GP9VeivFyobqzYDUOSeFFAWokI/edit"
+            target="_blank"
+          >
+            Andisor's frontend task
+          </a>{" "}
+          by{" "}
+          <a href="https://www.linkedin.com/in/aikansh-garg/" target="_blank">
+            Aikansh Garg.
+          </a>
+        </div>
+
+        <ProductList />
+      </div>
+    )
   );
 }
 
